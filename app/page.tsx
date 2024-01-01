07d3ec1d7548f5677a2727dc5a06a9c187353de1
Mateo Ortegon <60627969+blacksheep-git@users.noreply.github.com>
2024-01-01 11:56:14 -0700

    Completed initial about me.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import HeroSection from "@/app/components/HeroSection";
 import NavBar from "@/app/components/NavBar";
 import AboutSection from "@/app/components/AboutSection";
+import ProjectSection from "@/app/components/ProjectSection";
 
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
         <div className="container mt-24 mx-auto px-12 py-4">
             <HeroSection/>
             <AboutSection/>
+            <ProjectSection/>
         </div>
     </main>
   )
